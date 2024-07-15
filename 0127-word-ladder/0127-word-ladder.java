437cb8dfb3928b2c06c0628a8b6726276c3089ff
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-07-15 16:59:07 -0700

    Time: 585 ms (15.16%), Space: 44.5 MB (96.47%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,50 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        List<String> lst = new ArrayList<>(wordList);
+        ArrayDeque<String> ad = new ArrayDeque<>();
+        ad.add(beginWord);
+        int count = 1;
+        // String current = beginWord;
+        while(!wordList.isEmpty()){
+            count++;
+            int size = ad.size();
+            for (int i = 0; i < size; i++) {
+                Iterator<String> it = lst.iterator();
+                String before = ad.pollFirst();
+                while(it.hasNext()){
+                    String  after = it.next();
+                    if(helpFunction(before,after)){
+                        ad.addLast(after);
+                        it.remove();
+                        if(after.equals(endWord)){
+                            return count;
+                        }
+                    }
+
+                }
+
+            }
+            if(ad.isEmpty()){
+                return 0;
+            }
+        }
+        return 0;
+
+    }
+    boolean helpFunction( String before, String after){
+        int length = before.length();
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            if(before.charAt(i)!=after.charAt(i)){
+                count++;
+            }
+        }
+        return count==1;
+    }
+
+}
