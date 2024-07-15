1192a30de3403e5cf8f85f4c443141e3a874b7d3
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-07-15 15:29:17 -0700

    Time: 1 ms (84.42%), Space: 40.5 MB (44.86%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -5,32 +5,33 @@ class Solution {
         if(n == 0){
             return 1;
         }
-        HashMap<Integer, Integer> hm  = new HashMap<>();
+        int[] hm = new int[n];
         int count = 0;
         for (int i = 0; i < n; i++) {
-            hm.put(0,i);
+            //hm.put(0,i);
+            hm[0] = i;
             HelperFunction(hm, count+1,n);
-            hm.remove(0);
+            hm[0] = 0;
         }
         return result;
     }
-    void HelperFunction(HashMap<Integer, Integer> hm, int count, int n ){
+    void HelperFunction(int[] hm, int count, int n ){
             if(count == n){
                 result++;
             }else{
                 for (int pos = 0; pos < n; pos++) {
                     boolean qualified = true;
                     for (int i = 0; i < count; i++) {
-                        int temPos = hm.get(i);
+                        int temPos = hm[i];
                         if(temPos == pos || (count - i) == (pos - temPos)||(count - i) == -(pos - temPos) ){
                             qualified = false;
                             break;
                         }  
                     }
                     if(qualified){
-                        hm.put(count,pos);
+                        hm[count] = pos;
                         HelperFunction(hm, count + 1,n);
-                        hm.remove(count);
+                        hm[count] = 0;
                     }
                 }
                     
