27eb9cb0d82b90bbf745ab67b8619ee93b970d9d
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-07-16 15:07:12 -0700

    Time: 103 ms (21.54%), Space: 47.2 MB (5.13%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,166 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.length() > s.length()) {
+            return "";
+        }
+        int[] dir = new int[52];
+        for (int i = 0; i < dir.length; i++) {
+            dir[i] = Integer.MIN_VALUE;
+        }
+        int tlength = t.length();
+        for (int i = 0; i < t.length(); i++) {
+            if (t.charAt(i) >= 'a' && t.charAt(i) <= 'z') {
+                if (dir[t.charAt(i) - 'a'] == Integer.MIN_VALUE) {
+                    dir[t.charAt(i) - 'a'] = 1;
+                } else {
+                    dir[t.charAt(i) - 'a']++;
+                }
+            } else {
+                if (dir[t.charAt(i) - 'A' + 26] == Integer.MIN_VALUE) {
+                    dir[t.charAt(i) - 'A' + 26] = 1;
+                } else {
+                    dir[t.charAt(i) - 'A' + 26]++;
+                }
+            }
+        }
+        int[] maxArray = dir.clone();
+        int count = 0;
+        int left = 0;
+        int right = tlength - 1;
+        int maxLength = Integer.MAX_VALUE;
+        String maxString = "";
+        for (int i = 0; i < t.length(); i++) {
+            if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
+                if (dir[s.charAt(i) - 'a'] == Integer.MIN_VALUE) {
+                     if (count == t.length()) {
+                            if (right - left + 1 < maxLength) {
+                                 maxLength = right - left + 1;
+                                maxString = s.substring(left, right+1);
+                            }
+                    }
+                } else {
+                    dir[s.charAt(i) - 'a']--;
+                    if (dir[s.charAt(i) - 'a'] >= 0) {
+                        count++;
+                        if (count == t.length()) {
+                            if (right - left + 1 < maxLength) {
+                                 maxLength = right - left + 1;
+                                maxString = s.substring(left, right+1);
+                            }
+                        }
+                    }
+                }
+            } else {
+                if (dir[s.charAt(i) - 'A' + 26] == Integer.MIN_VALUE) {
+                     if (count == t.length()) {
+                            if (right - left + 1 < maxLength) {
+                                 maxLength = right - left + 1;
+                                maxString = s.substring(left, right+1);
+                            }
+                        }
+                } else {
+                    dir[s.charAt(i) - 'A'+ 26]--;
+                    if (dir[s.charAt(i) - 'A'+ 26] >= 0) {
+                        count++;
+                        if (count == t.length()) {
+                            if (right - left + 1 < maxLength) {
+                                 maxLength = right - left + 1;
+                                maxString = s.substring(left, right+1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        while (right < s.length()) {
+            if (count == t.length()) {
+                int originLeft = left;
+                left++;
+                if (s.charAt(originLeft) >= 'a' && s.charAt(originLeft) <= 'z') {
+                    if (dir[s.charAt(originLeft) - 'a'] == Integer.MIN_VALUE) {
+                         if (count == t.length()) {
+                            if (right - left + 1 < maxLength) {
+                                 maxLength = right - left + 1;
+                                maxString = s.substring(left, right+1);
+                            }
+                        }
+                    } else {
+                        dir[s.charAt(originLeft) - 'a']++;
+                        if (dir[s.charAt(originLeft) - 'a'] > 0) {
+                            count--;
+                        }
+                    }
+                    if (count == t.length()) {
+                        if (right - left + 1 < maxLength) {
+                            maxLength = right - left + 1;
+                            maxString = s.substring(left, right+1);
+                        }
+                    }
+                } else {
+                    if (dir[s.charAt(originLeft) - 'A'+ 26] == Integer.MIN_VALUE) {
+                         if (count == t.length()) {
+                            if (right - left + 1 < maxLength) {
+                                 maxLength = right - left + 1;
+                                maxString = s.substring(left, right+1);
+                            }
+                        }
+                    } else {
+                        dir[s.charAt(originLeft) - 'A'+ 26]++;
+                        if (dir[s.charAt(originLeft) - 'A'+ 26] > 0) {
+                            count--;
+                        }
+                    }
+                    if (count == t.length()) {
+
+                        if (right - left + 1 < maxLength) {
+                            maxLength = right - left + 1;
+                            maxString = s.substring(left, right+1);
+                        }
+                    }
+                }
+            } else {
+                right++;
+                if (right >= s.length()) {
+                    return maxString;
+                } else {
+                    if (s.charAt(right) >= 'a' && s.charAt(right) <= 'z') {
+                        if (dir[s.charAt(right) - 'a'] == Integer.MIN_VALUE) {
+                            continue;
+                        } else {
+                            dir[s.charAt(right) - 'a']--;
+                            if (dir[s.charAt(right) - 'a'] >= 0) {
+                                count++;
+                                if (count == t.length()) {
+                                    if (right - left + 1 < maxLength) {
+                                        maxLength = right - left + 1;
+                                        maxString = s.substring(left, right+1);
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        if (dir[s.charAt(right) - 'A' + 26] == Integer.MIN_VALUE) {
+                            continue;
+                        } else {
+                            dir[s.charAt(right) - 'A'+ 26]--;
+                            if (dir[s.charAt(right) - 'A'+ 26] >= 0) {
+                                count++;
+                                if (count == t.length()) {
+                                    if (right - left + 1 < maxLength) {
+                                        maxLength = right - left + 1;
+                                        maxString = s.substring(left, right+1);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
+        return maxString;
+    }
+}
