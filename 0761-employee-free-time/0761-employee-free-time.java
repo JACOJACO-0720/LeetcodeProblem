5181b38ce46c9886c75992bc517d0ee8b654581c
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-07-31 23:32:36 -0700

    Time: 13 ms (51.72%), Space: 45.6 MB (87.61%) - LeetHub

---
+++ 0761-employee-free-time/0761-employee-free-time.java
@@ -0,0 +1,49 @@
+
+
+import static java.util.Collections.frequency;
+
+/*
+// Definition for an Interval.
+class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {}
+
+    public Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
+class Solution {
+    public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
+        List<Interval> res  = new ArrayList<>();
+        List<Interval> flat = new ArrayList<>();
+        for (int i = 0; i < schedule.size(); i++) {
+            for (int j = 0; j < schedule.get(i).size(); j++) {
+                flat.add(schedule.get(i).get(j));
+            }
+        }
+        Collections.sort(flat,  (n1,n2) -> (n1.start - n2.start) );
+
+        if(flat.isEmpty()){
+            return null;
+        }
+        int curstart = flat.get(0).start; int curend = flat.get(0).end;
+
+        for (int i = 1; i < flat.size(); i++) {
+            Interval tmpInterval = flat.get(i);
+            if(tmpInterval.start>curend){
+                res.add(new Interval(curend, tmpInterval.start));
+                curstart = tmpInterval.start;
+                curend = tmpInterval.end;
+            }else{
+                curend = Math.max(curend, tmpInterval.end);
+            }
+        }
+        return res;
+
+    }
+}
