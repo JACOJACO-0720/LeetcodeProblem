8a09a1239fa646ddd7399725d162485e90d054d3
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-06 23:19:35 -0400

    Time: 9 ms (18.44%), Space: 45.6 MB (80.14%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,49 +1,58 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<Integer>[] adj = new List[numCourses];
-        int[] indegree = new int[numCourses];
-        List<Integer> ans = new ArrayList<>();
-
-        for (int[] pair : prerequisites) {
-            int course = pair[0];
-            int prerequisite = pair[1];
-            if (adj[prerequisite] == null) {
-                adj[prerequisite] = new ArrayList<>();
-            }
-            adj[prerequisite].add(course);
-            indegree[course]++;
-        }
-
-        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> res = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>(numCourses);
+        List<List<Integer>> revlist = new ArrayList<>(numCourses);
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                queue.offer(i);
-            }
+            list.add(new ArrayList<>());
+            revlist.add(new ArrayList<>());
         }
 
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            ans.add(current);
+        for (int[] temp: prerequisites ) {
+            list.get(temp[0]).add(temp[1]);
+            revlist.get(temp[1]).add(temp[0]);
+        }
 
-            if (adj[current] != null) {
-                for (int next : adj[current]) {
-                    indegree[next]--;
-                    if (indegree[next] == 0) {
-                        queue.offer(next);
+
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        for (int i = 0;i<list.size();i++) {
+            if(list.get(i).isEmpty()){
+                ad.add(i);
+            }
+        }
+        while(!ad.isEmpty()){
+            int size = ad.size();
+            for (int i = 0; i < size; i++) {
+                int tempint = ad.poll();
+                res.add(tempint);
+                List<Integer> tmpreverse = revlist.get(tempint);
+                if(tmpreverse.isEmpty()){
+                    continue;
+                }else{
+                    for (int j = 0; j < tmpreverse.size() ;j++) {
+                        int temprevint = tmpreverse.get(j);
+                        list.get(temprevint).remove(Integer.valueOf(tempint));
+                        if(list.get(temprevint).isEmpty()){
+                            ad.add(temprevint);
+                        }
                     }
                 }
             }
-        }
-         int[] tmp;
-        if(ans.size() != numCourses){
-            tmp = new int[0];
-            return tmp;
-        }
-        tmp = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++) {
-            tmp[i] = ans.get(i);
-        }
-        return tmp;
 
+        }
+        if(res.size()!=numCourses){
+            return new int[0];
+        }else{
+            int[] resarray = new int[numCourses];
+            for (int i = 0; i < resarray.length; i++) {
+                resarray[i]= res.get(i);
+            }
+            return resarray;
+        }
     }
 }
