8ed23544c752eaa8d653fa15be812f436270259a
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-07 21:34:36 -0400

    Time: 2 ms (13.32%), Space: 40.7 MB (60.11%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,57 +1,52 @@
 class Solution {
     public int reverse(int x) {
-        boolean isnegative = false;
+        if(x==0){
+            return 0;
+        }
+        boolean ispositive = false;
+        if(x>0){
+            ispositive = true;
+        }
+        x=Math.abs(x);
+        StringBuilder sb = new StringBuilder( String.valueOf(x));
 
-        if (x < 0) {
-            isnegative = true;
+        sb = sb.reverse();
+        while(sb.charAt(0)=='0'){
+            sb.deleteCharAt(0);
+        }
+        if(sb.length()<10){
+            int flag = ispositive?1:-1;
+            return Integer.valueOf(sb.toString())*flag;
+        }else{
+            if(ispositive){
+                String max = String.valueOf(Integer.MAX_VALUE);
+                for (int i = 0; i < max.length(); i++) {
+                    if(max.charAt(i)<sb.charAt(i)){
+                        return 0;
+                    }else if(max.charAt(i)==sb.charAt(i)){
+                        continue;
+                    }else{
+                        return  Integer.valueOf(sb.toString());
+                    }
+                    
+                }
+                return  Integer.valueOf(sb.toString());
+            }else{
+                String max = String.valueOf(Integer.MIN_VALUE).substring(1);
+                for (int i = 0; i < max.length(); i++) {
+                    if(max.charAt(i)<sb.charAt(i)){
+                        return 0;
+                    }else if(max.charAt(i)==sb.charAt(i)){
+                        continue;
+                    }else{
+                        return  Integer.valueOf(sb.toString())*(-1);
+                    }
+                   
+                }
+                 return  Integer.valueOf(sb.toString())*(-1);
+            }
         }
 
-        String str = Integer.toString(x);
-        String maxIntegerstr = String.valueOf(Integer.MAX_VALUE);
-        String minIntegerstr = String.valueOf(Integer.MIN_VALUE);
-        if (isnegative) {
-            if (str.length() == minIntegerstr.length()) {
-                for (int i = 1; i < str.length(); i++) {
-                    if (minIntegerstr.charAt(i) < str.charAt(str.length() - i)) {
-                        return 0;
-                    }else if(minIntegerstr.charAt(i) > str.charAt(str.length() - i)){
-                        break;
-                    }
-                }
-            }
-            StringBuilder sb = new StringBuilder(str);
-            int left = 1;
-            int right = sb.length() - 1;
-            while (left < right) {
-                char temp = sb.charAt(left);
-                sb.setCharAt(left, sb.charAt(right));
-                sb.setCharAt(right, temp);
-                left++;
-                right--;
-            }
-            return Integer.valueOf(sb.toString());
-        } else {
-            if (str.length() == maxIntegerstr.length()) {
-                for (int i = 0; i < str.length(); i++) {
-                    if (maxIntegerstr.charAt(i) < str.charAt(str.length() - 1 - i)) {
-                        return 0;
-                    }else if(maxIntegerstr.charAt(i) > str.charAt(str.length() -1- i)){
-                        break;
-                    }
-                }
-            }
-            StringBuilder sb = new StringBuilder(str);
-            int left = 0;
-            int right = sb.length() - 1;
-            while (left < right) {
-                char temp = sb.charAt(left);
-                sb.setCharAt(left, sb.charAt(right));
-                sb.setCharAt(right, temp);
-                left++;
-                right--;
-            }
-            return Integer.valueOf(sb.toString());
-        }
 
     }
 }
