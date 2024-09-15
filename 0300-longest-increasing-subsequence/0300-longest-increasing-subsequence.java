8941193f0166019cbda4a41ad5fea2a6e4d283ce
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-15 18:24:02 -0400

    Time: 212 ms (18.63%), Space: 43.7 MB (94.58%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        return dfs(dp, 0 ,nums, Integer.MIN_VALUE);
+
+    }
+    int dfs(int[] dp, int x, int[] nums, int prev){
+        if(x== nums.length){
+            return 0;
+        }
+
+        if(nums[x]<=prev){
+            return dfs(dp, x+1, nums , prev) ;
+        }else{
+            if(dp[x]!=0){
+                return Math.max(dp[x], dfs(dp, x+1, nums , prev)) ;
+            }else{
+                dp[x] = Math.max(dfs(dp, x+1, nums , nums[x])+1, dfs(dp, x+1, nums , prev)) ;
+                return dp[x];
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] input = {10,9,2,5,3,7,101,18};
+        System.out.println(solution.lengthOfLIS(input));
+
+    }
+}
