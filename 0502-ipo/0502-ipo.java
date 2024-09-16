9a6a50445026af7cf0edbbf69271daf8a58ddf19
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-16 13:53:57 -0400

    Time: 103 ms (43.27%), Space: 64.1 MB (29.54%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,65 +1,29 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        // Min-heap for projects based on required capital
-        PriorityQueue<Node> minCapitalPQ = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node n1, Node n2) {
-                return Integer.compare(n1.capital, n2.capital);
-            }
-        });
+        int count = 0;
+        int[][] pc = new int[profits.length][2];
+        for (int i = 0; i < pc.length; i++) {
+            pc[i][0] = capital[i];
+            pc[i][1] = profits[i];
 
-        // Max-heap for projects based on profit
-        PriorityQueue<Node> maxProfitPQ = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node n1, Node n2) {
-                return Integer.compare(n2.profit, n1.profit);
-            }
-        });
-
-        // Add all projects to the min-heap based on capital requirements
-        for (int i = 0; i < profits.length; i++) {
-            minCapitalPQ.add(new Node(profits[i], capital[i]));
         }
+        int posi = 0;
+        Arrays.sort(pc, (int[] n1, int[] n2)->Integer.compare(n1[0], n2[0]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((int[] n1, int[] n2)->Integer.compare(n2[1], n1[1]));
+        while(count<k){
 
-        int currentCapital = w;
-
-        // Select up to k projects
-        for (int i = 0; i < k; i++) {
-            // Move all projects that can be started with current capital to the max-heap
-            while (!minCapitalPQ.isEmpty() && minCapitalPQ.peek().capital <= currentCapital) {
-                maxProfitPQ.add(minCapitalPQ.poll());
+            while(posi<pc.length && w >= pc[posi][0]){
+                pq.add(pc[posi]);
+                posi++;
             }
-
-            // If there are no available projects, break
-            if (maxProfitPQ.isEmpty()) {
+            if(pq.isEmpty()){
                 break;
             }
+            int[] temp = pq.poll();
+            w += temp[1];
+            count++;
 
-            // Select the project with the highest profit
-            currentCapital += maxProfitPQ.poll().profit;
         }
-
-        return currentCapital;
-    }
-
-    // Static nested Node class
-    static class Node {
-        int profit;
-        int capital;
-
-        public Node(int profit, int capital) {
-            this.profit = profit;
-            this.capital = capital;
-        }
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] capital = {0, 1, 1};
-        int[] profit = {1, 2, 3};
-        System.out.println(solution.findMaximizedCapital(2, 0, profit, capital)); // Output should be the maximum capital after selecting up to k projects
+        return w;
     }
 }
