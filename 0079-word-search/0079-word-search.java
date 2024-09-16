009166872c0eed4883fb190dddd649233b2f522d
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-15 21:56:16 -0400

    Time: 268 ms (13.25%), Space: 45 MB (13.23%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,50 +1,48 @@
 class Solution {
+    private int rows;
+    private int cols;
+    private Set<String> visited;
+
     public boolean exist(char[][] board, String word) {
-        boolean[][] visited = new boolean[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, word, i, j, visited)) {
+        rows = board.length;
+        cols = board[0].length;
+        visited = new HashSet<>();
+
+        Map<Character, Integer> count = new HashMap<>();
+        for (char c : word.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        if (count.getOrDefault(word.charAt(0), 0) > count.getOrDefault(word.charAt(word.length() - 1), 0)) {
+            word = new StringBuilder(word).reverse().toString();
+        }
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (dfs(board, word, r, c, 0)) {
                     return true;
                 }
             }
         }
+
         return false;
     }
 
-    boolean dfs(char[][] board, String word, int x, int y, boolean[][] visited) {
-        if ( outofbound(board, x, y)||visited[x][y] ) {
-            return false;
-        }
-        if (word.length() == 1 && word.equals(String.valueOf(board[x][y]))) {
+    private boolean dfs(char[][] board, String word, int r, int c, int k) {
+        if (k == word.length()) {
             return true;
         }
-        if (word.length() == 1) {
+
+        if (r < 0 || r >= rows || c < 0 || c >= cols || visited.contains(r + "," + c) || board[r][c] != word.charAt(k)) {
             return false;
         }
 
-        if (word.charAt(0) == board[x][y]) {
-            visited[x][y] = true;
-            boolean res = dfs(board, word.substring(1), x + 1, y, visited) ||
-                    dfs(board, word.substring(1), x, y - 1, visited) ||
-                    dfs(board, word.substring(1), x, y + 1, visited) ||
-                    dfs(board, word.substring(1), x - 1, y, visited);
-            if (res) {
-                return true;
-            } else {
-                visited[x][y] = false;
-                return false;
-            }
-        }else{
-            return false;
-        }
-
-
-    }
-
-    boolean outofbound(char[][] board, int x, int y) {
-        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) {
-            return true;
-        }
-        return false;
-    }
+        visited.add(r + "," + c);
+        boolean res = dfs(board, word, r + 1, c, k + 1) ||
+                      dfs(board, word, r - 1, c, k + 1) ||
+                      dfs(board, word, r, c + 1, k + 1) ||
+                      dfs(board, word, r, c - 1, k + 1);
+        visited.remove(r + "," + c);
+        return res;
+    }    
 }
