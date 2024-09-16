194552b78a9c64e65591557c0eb2afa77282a423
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-15 23:00:00 -0400

    Time: 1 ms (11.39%), Space: 47.6 MB (5.13%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -15,41 +15,30 @@
  */
 class Solution {
     public int maxPathSum(TreeNode root) {
-        int[] res =  helperFunction(root);
-        return Math.max(res[0], res[1]);
+        if(root==null){
+            return 0;
+        }
+        int[] res= dfs(root);
+        return Math.max(res[0],res[1]);
     }
 
-
-    //1 cannot cat 2 can cat
-    public int[] helperFunction(TreeNode root){
-        if(root.left==null&&root.right==null){
-            return new int[]{root.val,root.val};
-        }else if(root.left==null){
-            int[] tmp =  helperFunction(root.right);
-            int cat = Math.max(tmp[1]+root.val, root.val);
-            int notcat = tmp[0];
-            return new int[]{Math.max(notcat,cat), cat };
-        }else if(root.right==null){
-            int[] tmp =  helperFunction(root.left);
-            int cat = Math.max(tmp[1]+root.val, root.val);
-            int notcat = tmp[0];
-            return new int[]{Math.max(notcat,cat), cat};
+    int[] dfs(TreeNode root){
+        if(root==null){
+            int[] temp = new int[2];
+            temp[0] = -1000;
+            temp[1] = -1000;
+            return temp;
         }else{
-            int[] tmpleft =  helperFunction(root.left);
-            int[] tmpright =  helperFunction(root.right);
-            //not cat
-            int leftleft = tmpleft[0];
-            int rightright = tmpright[0];
-            int leftmidright = tmpleft[1]+ root.val + tmpright[1];
-            int notcat = Math.max(leftleft,rightright );
-            notcat = Math.max(notcat,leftmidright );
-            //cat
-            int leftright = tmpleft[1];
-            int rightleft = tmpright[1];
-            int cat;
-            cat = Math.max(leftright+root.val,rightleft+root.val);
-            cat = Math.max(cat,root.val);
-            return new int[]{Math.max(cat, notcat), cat};
+            //for global
+            int[] left = dfs(root.left);
+            int[] right = dfs(root.right);
+            //for local
+            int global = Math.max(root.val, Math.max(root.val+left[0], root.val+right[0]));
+            int local = Math.max(Math.max(global, root.val+left[0]+right[0]), Math.max(right[1], left[1]));
+            int[] res = new int[2];
+            res[0] = global;
+            res[1] = local;
+            return res;
         }
     }
 }
