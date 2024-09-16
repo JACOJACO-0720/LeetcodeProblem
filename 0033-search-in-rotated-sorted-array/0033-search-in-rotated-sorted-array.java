0cc6d9f9146d633e08ca96db21bf21288ffdfc9b
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-16 17:06:10 -0400

    Time: 0 ms (100%), Space: 42 MB (40.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,36 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0;int right = nums.length-1;
-        while(right>=left){
-            int mid = left+(right-left)/2;
+        int left = 0; int right =nums.length-1;
+        int leftleft = nums[0]; int rightright = nums[nums.length-1];
+        while(left<=right){
+            int mid = left +(right-left)/2;
             if(nums[mid]==target){
                 return mid;
             }else{
-                if(nums[mid]>=nums[left]&&nums[mid]>nums[right]){
-                    if(nums[mid]<target){
+                if(nums[mid]<target){
+                    if(nums[mid]>=leftleft && target>=leftleft){
+                        left=mid+1;
+                    }else if(nums[mid]<=rightright && target<=rightright){
                         left = mid+1;
-                        continue;
                     }else{
-                        if(target>=nums[left]){
-                            right = mid-1;
-                            continue;
-                        }else{
-                            left = mid+1;
-                            continue;
-                        }
+                        right = mid-1;
                     }
                 }else{
-                    if(nums[mid]>target){
+                    if(nums[mid]>=leftleft && target>=leftleft){
                         right = mid-1;
-                        continue;
+                    }else if(nums[mid]<=rightright && target<=rightright){
+                         right = mid-1;
                     }else{
-                        if(target<=nums[right]){
-                            left = mid+1;
-                            continue;
-                        }else{
-                            right = mid-1;
-                            continue;
-                        }
+                        left = mid+1;
                     }
                 }
             }
