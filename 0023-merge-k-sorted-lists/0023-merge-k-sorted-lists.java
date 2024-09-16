f978e97250f556ad2be95cfe1f033354f030e80c
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-15 22:27:02 -0400

    Time: 4 ms (74.14%), Space: 44.4 MB (53.74%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,5 +1,4 @@
-
-import java.util.Collection;/**
+/**
  * Definition for singly-linked list.
  * public class ListNode {
  *     int val;
@@ -10,31 +9,35 @@ import java.util.Collection;/**
  * }
  */
 class Solution {
-    // current  4, 2, 2
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode res = null;
-        ListNode head  = null;
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((ListNode ln1, ListNode ln2) -> Integer.compare( ln1.val,ln2.val));
-        for (int i = 0; i < lists.length; i++) {
-            if(lists[i]!=null){
+            PriorityQueue<ListNode> pq = new PriorityQueue<>((ListNode n1, ListNode n2)->(Integer.compare(n1.val, n2.val)));
+            ListNode res =null;
+            ListNode current = null;
+            if(lists.length==0){
+                return null;
+            }
+            for (int i = 0; i < lists.length; i++) {
+                if(lists[i]==null){
+                    continue;
+                }
                 pq.add(lists[i]);
             }
-        }
+            while(!pq.isEmpty()){
+                ListNode temp = pq.poll();
+                if(res==null){
+                    res = temp;
+                    current = temp;
+                }else{
+                    current.next = temp;
+                    current = current.next;
+                }
+                ListNode a = current.next;
+                current.next =null;
+                if(a!=null){
+                    pq.add(a);
+                }
 
-        while(!pq.isEmpty()){
-            ListNode small = pq.poll();
-            if(small.next!=null){
-                pq.add(small.next);
             }
-            if(res==null){
-                res = small;
-                head = small;
-            }else{
-                res.next = small;
-                res = small;
-            }
-        }
-
-        return head;
+                return res;
     }
 }
