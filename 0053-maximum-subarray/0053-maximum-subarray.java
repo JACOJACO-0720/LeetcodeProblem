ea7d0d0ea23f1b69a73eedf31b1ce7eef9fde878
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-16 20:30:03 -0400

    Time: 1 ms (99.39%), Space: 56.7 MB (89.28%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,24 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        if(nums.length==1)return nums[0];
-        int SumMax = nums[0];
-        int curMax = nums[0]<0?0:nums[0];
+        int max = nums[0];
+        int localmax = nums[0]<0?0:nums[0];
         for (int i = 1; i < nums.length; i++) {
-            if(nums[i]<0){
-                curMax = curMax +nums[i];
-                if(curMax>SumMax){
-                    SumMax = curMax;
+            localmax = localmax+nums[i];
+if(localmax>max){
+                    max = localmax;
                 }
-                if(curMax<0){
-                    curMax = 0;
-                }
-            }else{
-                curMax = curMax +nums[i];
-                if(curMax>SumMax){
-                    SumMax = curMax;
-                }
+            if(localmax<0){
+                localmax = 0;
             }
+
         }
-        return SumMax;
+        return max;
     }
 }
