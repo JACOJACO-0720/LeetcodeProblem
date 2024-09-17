bbc3acc4db8c1d1fa16bb0745c6ca0adce124fc1
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-16 21:04:01 -0400

    Time: 4 ms (85.51%), Space: 49.1 MB (53.74%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.java
@@ -1,73 +1,52 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        if(nums.length==1)return nums[0];
-
-        int sum = 0;
-        for(int num:nums){
-            sum+=num;
-        }
-        int SumMax = nums[0];
-        int curMax = nums[0]<0?0:nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            if(nums[i]<0){
-                curMax = curMax +nums[i];
-                if(curMax>SumMax){
-                    SumMax = curMax;
-                }
-                if(curMax<0){
-                    curMax = 0;
-                }
-            }else{
-                curMax = curMax +nums[i];
-                if(curMax>SumMax){
-                    SumMax = curMax;
-                }
-            }
-        }
-
-        if(nums.length==1)return nums[0];
-
-        int length =1;;
-        int SumMin = nums[0];
-        int curMin;
-        int minmin = nums[0];
-        if(nums[0]>0){
-            curMin = 0;
-            length =0;
-        }else{
-            curMin = nums[0];
-
-        }
-        for (int i = 1; i < nums.length; i++) {
-            if(minmin>nums[i]){
-                minmin = nums[i];
-            }
-            if(nums[i]>0){
-                length++;
-                curMin = curMin + nums[i];
-                if(curMin<SumMin){
-                    SumMin = curMin;
-                }
-                if(curMin > 0){
-                    curMin = 0;
-                    length = 0;
-                }
-            }else{
-                length++;
-                curMin = curMin + nums[i];
-                if(curMin < SumMin){
-                    SumMin = curMin;
-                }
-            }
-        }
-
-        int sec;
-        if(length == nums.length){
-            sec = sum - minmin;
-        }else{
-            sec = sum-SumMin;
-        }
-
-        return Math.max(SumMax,sec);
+        int middle = findmiddle(nums);
+        int sides = findsides(nums);
+        return Math.max(middle, sides);
     }
+
+    int findmiddle(int[] nums){
+        int sum = nums[0];
+        int cursum = nums[0]<0?0:nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            cursum+=nums[i];
+            if(cursum>sum){
+                sum = cursum;
+            }
+            if(cursum<0){
+                cursum = 0;
+            }
+        }
+        return sum;
+    }
+    
+     int findsides(int[] nums){
+        int sum = nums[0];
+        int cursum = nums[0]>0?0:nums[0];
+        int length = nums[0]>0?0:1;
+        for (int i = 1; i < nums.length; i++) {
+
+            cursum+=nums[i];
+            length++;
+            if(cursum<sum){
+                sum = cursum;
+            }
+            if(cursum>0){
+                length = 0;
+                cursum = 0;
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        int revsum = 0;
+        for(int num:nums){
+            if(num<min){
+                min = num;
+            }
+            revsum+=num;
+        }
+        if(length!=nums.length){
+             return revsum-sum;
+        }
+       return revsum-sum + min;
+     }
 }
