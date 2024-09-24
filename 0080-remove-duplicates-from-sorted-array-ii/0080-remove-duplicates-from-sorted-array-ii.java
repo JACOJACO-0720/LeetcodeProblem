61643d54b761e49af62179a67760650002daf8e9
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-24 19:42:45 -0400

    Time: 0 ms (100%), Space: 44.4 MB (68.48%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,8 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int i = 0;
-        for (int n : nums) 
-            if (i < 2 || n > nums[i-2]) nums[i++] = n; 
+        for (int n : nums) {
+            if (i < 2 || n > nums[i-2]) {
+                    nums[i++] = n; 
+            }
+        }
+
+
         
         return i;
     }
