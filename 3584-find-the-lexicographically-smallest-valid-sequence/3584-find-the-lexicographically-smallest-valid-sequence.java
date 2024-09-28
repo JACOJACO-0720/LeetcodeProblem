b40400a39a166c56c7970997634dad3a887e93dc
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-28 13:30:52 -0400

    Time: 37 ms (50%), Space: 77.7 MB (50%) - LeetHub

--- 3584-find-the-lexicographically-smallest-valid-sequence/3584-find-the-lexicographically-smallest-valid-sequence.java
+++ 3584-find-the-lexicographically-smallest-valid-sequence/3584-find-the-lexicographically-smallest-valid-sequence.java
@@ -1,41 +1,34 @@
 class Solution {
     public int[] validSequence(String word1, String word2) {
-        int i = 0, j = 0;
-        int[] res = new int[word2.length()];
-        while (i < word1.length() && j < word2.length() && word1.charAt(i) == word2.charAt(j)) {
-            res[j++] = i++;
-        }
-        while (i < word1.length() && j < word2.length()) {
-            if (find(word1, word2, i + 1, j + 1, res)) {
-                res[j] = i;
-                return res;
-            }
-            while (i < word1.length()) {
-                if (word1.charAt(i) == word2.charAt(j)) {
-                    while (i < word1.length() && j < word2.length() && word1.charAt(i) == word2.charAt(j)) {
-                        res[j++] = i++;
-                    }
-                    break;
-                } else {
-                    i++;
-                }
-            }
-        }
-        if (j < word2.length()) {
-            return new int[]{};
-        }
-        return res;
-    }
-
-    static boolean find(String w1, String w2, int i, int j, int[] res) {
-        while (i < w1.length() && j < w2.length()) {
-            if (w1.charAt(i) == w2.charAt(j)) {
-                res[j++] = i++;
+        int len1 = word1.length();
+        int len2 = word2.length();
+        // longestSuffix[i] := the longest length of suffix of word2 in word1[i..]
+        int[] longestSuffix = new int[len1 + 1];
+        int i2 = len2 - 1;
+        for (int i1 = len1 - 1; i1 >= 0; i1--) {
+            if (i2 >= 0 && word1.charAt(i1) == word2.charAt(i2)) {
+                longestSuffix[i1] = len2 - i2;
+                i2--;
             } else {
-                i++;
+                longestSuffix[i1] = i1 < len1 - 1 ? longestSuffix[i1 + 1] : 0;
             }
         }
-        return j == w2.length();
+        // Compute the result with longestSuffix
+        int[] result = new int[len2];
+        int matchedLen = 0;
+        boolean replaced = false;
+        for (int i1 = 0; i1 < len1; i1++) {
+            if (word1.charAt(i1) == word2.charAt(matchedLen)) {
+                result[matchedLen] = i1;
+                matchedLen++;
+            } else if (!replaced && longestSuffix[i1 + 1] >= len2 - matchedLen - 1) {
+                // the condition means there exists a solution by replacing words1[i1] 
+                replaced = true;
+                result[matchedLen] = i1;
+                matchedLen++;
+            }
+            if (matchedLen == word2.length()) return result;
+        }
+        return new int[]{};
     }
-
 }
