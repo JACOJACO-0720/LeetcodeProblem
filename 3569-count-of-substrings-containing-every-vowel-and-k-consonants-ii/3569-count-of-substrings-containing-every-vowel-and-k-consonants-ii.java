5f3f6cfeb0076b4bf4e486d2677bddd15edd6774
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-09-29 00:42:25 -0400

    Time: 163 ms (100%), Space: 46.5 MB (100%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -1,43 +1,52 @@
 class Solution {
-    public long count(String word, int k) {
-        if (k < 0) {
-            return 0;
-        }
-        
-        Map<Character, Integer> hmap = new HashMap<>();
-        int cons = 0;
-        int left = 0;
+    public long countOfSubstrings(String word, int k) {
         long ans = 0;
-        String vowel = "aeiou";
-        int n = word.length();
-        for (int right = 0; right < word.length(); right++) {
-            char currentChar = word.charAt(right);
-            if (vowel.indexOf(currentChar) >= 0) {
-                hmap.put(currentChar, hmap.getOrDefault(currentChar, 0) + 1);
-            } else {
-                cons++;
+        int a = 0, e = 0, i = 0, o = 0, u = 0;
+        int left = 0;
+        // max length of follwing vowel string
+        long[] countlast = new long[word.length()];
+        for (int index = countlast.length - 1; index >= 0; index--) {
+            if (index == countlast.length - 1) {
+                countlast[index] = 0;
+            }  else {
+                if (word.charAt(index + 1) == 'a' || word.charAt(index + 1) == 'e' || word.charAt(index + 1) == 'i' || word.charAt(index + 1) == 'o' || word.charAt(index + 1) == 'u') countlast[index] = countlast[index + 1] + 1;
+                else countlast[index] = 0;
             }
-            while (cons >= k && hmap.getOrDefault('a', 0) > 0 &&
-                hmap.getOrDefault('e', 0) > 0 &&
-                hmap.getOrDefault('i', 0) > 0 &&
-                hmap.getOrDefault('o', 0) > 0 &&
-                hmap.getOrDefault('u', 0) > 0) {
-                    ans+=(n-right);
-                char leftChar = word.charAt(left);
-                if (vowel.indexOf(leftChar) >= 0) {
-                    hmap.put(leftChar, hmap.get(leftChar) - 1);
-                } else {
-                    cons--;
+        }
+
+        for (int right = 0; right < word.length(); right++) {
+            if (word.charAt(right) == 'a') a++;
+            if (word.charAt(right) == 'e') e++;
+            if (word.charAt(right) == 'i') i++;
+            if (word.charAt(right) == 'o') o++;
+            if (word.charAt(right) == 'u') u++;
+
+            while (a >= 1 && e >= 1 && i >= 1 && o >= 1 && u >= 1 && (right - left + 1) - a - e - i - o - u >= k) {
+                if ((right - left + 1) - a - e - i - o - u == k) {
+                    ans += countlast[right];
+                    ans++;
                 }
+                if (word.charAt(left) == 'a') a--;
+                if (word.charAt(left) == 'e') e--;
+                if (word.charAt(left) == 'i') i--;
+                if (word.charAt(left) == 'o') o--;
+                if (word.charAt(left) == 'u') u--;
                 left++;
             }
+
+        }
+
+        while (left < word.length()) {
+            if (a >= 1 && e >= 1 && i >= 1 && o >= 1 && u >= 1 && (word.length() - left) - a - e - i - o - u == k){
+                ans++;
+            }
+            if (word.charAt(left) == 'a') a--;
+            if (word.charAt(left) == 'e') e--;
+            if (word.charAt(left) == 'i') i--;
+            if (word.charAt(left) == 'o') o--;
+            if (word.charAt(left) == 'u') u--;
+            left++;
         }
         return ans;
-    }
-
-    public long countOfSubstrings(String word, int k) {
-        long val1 = count(word,k);
-        long val2 = count(word,k+1);
-        return val1-val2;
     }
 }
