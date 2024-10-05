d9e164efc6adb15c2a6c3bd0557881462cff6cb9
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-10-05 16:57:51 -0400

    Time: 4 ms (90.04%), Space: 45.8 MB (79.77%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,40 +1,33 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(new MyComparator());
-        for (int i = 0; i < intervals.length; i++) {
-            int[] temp = intervals[i];
-            int[] begin = new int[2];
-            begin[0] = temp[0];
-            int[] end = new int[2];
-            end[0] = temp[1];
-            end[1] = 1;
-            pq.add(begin);pq.add(end);
+        int n = intervals.length;
+        int[] startTimes = new int[n];
+        int[] endTimes = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            startTimes[i] = intervals[i][0];
+            endTimes[i] = intervals[i][1];
         }
-        int cur = 0;
-        int maxcur = 0;
-        while(!pq.isEmpty()){
-            int[] temp = pq.poll();
-            if(temp[1] ==0){
+        
+        Arrays.sort(startTimes);
+        Arrays.sort(endTimes);
+        
+        int res = 0, cur = 0;
+        int i = 0, j = 0;
+        
+        while (i < n) {
+            if (startTimes[i] < endTimes[j]) {
                 cur++;
-                maxcur = Math.max(cur,maxcur);
-            }else{
+                i++;
+            } else {
                 cur--;
+                j++;
             }
+            res = Math.max(res, cur);
         }
-        return maxcur;
-
-    }
-    class MyComparator implements Comparator<int[]> {
-        @Override
-        public int compare(int[] o1, int[] o2) {
-            int tmp = Integer.compare(o1[0], o2[0]);
-            if (tmp == 0) {
-                tmp = Integer.compare(o2[1], o1[1]);
-            }
-            return tmp;
-        }
+        
+        return res;
     }
 }
