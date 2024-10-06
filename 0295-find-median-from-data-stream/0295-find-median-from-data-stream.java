f257081244de5ef17805ae884094ff843e97567e
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-10-06 19:38:14 -0400

    Time: 119 ms (32.21%), Space: 63.8 MB (40.7%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -2,77 +2,26 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 
 class MedianFinder {
-    PriorityQueue<Integer> minStack = new PriorityQueue<>();
-    PriorityQueue<Integer> maxStack = new PriorityQueue<>(Collections.reverseOrder());
-    public MedianFinder() {
+    private PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
+    private PriorityQueue<Integer> large = new PriorityQueue<>();
+    private boolean even = true;
 
+    public double findMedian() {
+        if (even)
+            return (small.peek() + large.peek()) / 2.0;
+        else
+            return small.peek();
     }
 
     public void addNum(int num) {
-        if(minStack.isEmpty()&&maxStack.isEmpty()){
-            minStack.add(num);
-        }else if(minStack.isEmpty()){
-            int maxInt = maxStack.peek();
-            if(maxInt<=num){
-                minStack.add(num);
-            }else{
-                int temp = maxStack.poll();
-                minStack.add(temp);
-                maxStack.add(num);
-            }
-
-        }else if(maxStack.isEmpty()){
-            int minInt = minStack.peek();
-            if(minInt>=num){
-                maxStack.add(num);
-            }else{
-                int temp = minStack.poll();
-                maxStack.add(temp);
-                minStack.add(num);
-            }
-        }else{
-            if(maxStack.size() == minStack.size() ){
-                int minInt = minStack.peek();
-                if(minInt>=num){
-                    maxStack.add(num);
-                }else{
-                    minStack.add(num);
-                }
-            }else if(maxStack.size() > minStack.size()){
-                int maxInt = maxStack.peek();
-                if(maxInt<=num){
-                    minStack.add(num);
-                }else{
-                    int temp = maxStack.poll();
-                    minStack.add(temp);
-                    maxStack.add(num);
-                }
-
-            }else{
-                int minInt = minStack.peek();
-                if(minInt>=num){
-                    maxStack.add(num);
-                }else{
-                    int temp = minStack.poll();
-                    maxStack.add(temp);
-                    minStack.add(num);
-                }
-
-            }
+        if (even) {
+            large.offer(num);
+            small.offer(large.poll());
+        } else {
+            small.offer(num);
+            large.offer(small.poll());
         }
-    }
-
-    public double findMedian() {
-        if(minStack.size()==0&&maxStack.size()==0){
-            return 0;
-        }
-        if(minStack.size()==maxStack.size()){
-            return (minStack.peek()+maxStack.peek())/2.0;
-        }else if(minStack.size()>maxStack.size()){
-            return minStack.peek();
-        }else{
-            return maxStack.peek();
-        }
+        even = !even;
     }
 }
 
