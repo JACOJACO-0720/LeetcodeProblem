41be3a72d8994553c4fca9ce508c30a1e9beffce
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-10-06 15:08:53 -0400

    Time: 5 ms (42.75%), Space: 40.9 MB (99.76%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,55 +1,27 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-        if(s1.length()+s2.length()!=s3.length()){
+        if (s1.length() + s2.length() != s3.length()) {
             return false;
         }
-        int s1l = s1.length();
-        int s2l = s2.length();
-        int[][] dp = new int[s1l+1][s2l+1];
-        //init
-        dp[0][0] = 0;
-        int i = 0;
-        for (i=0; i < s1l; i++) {
-            if(s1.charAt(i) == s3.charAt(i)){
-                dp[i+1][0] = dp[i][0]+1;
-            }else{
-                break;
+
+        // dp 表示是否可以通过 s1 的前 i 个字符和 s2 的前 j 个字符拼接得到 s3 的前 i+j 个字符
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+
+        // 初始化起点
+        dp[0][0] = true;
+
+        // 填充 dp 表格
+        for (int i = 0; i <= s1.length(); i++) {
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i > 0) {
+                    dp[i][j] = dp[i][j] || (dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1));
+                }
+                if (j > 0) {
+                    dp[i][j] = dp[i][j] || (dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+                }
             }
         }
-        while(i < s1l){
-            dp[i+1][0] = dp[i][0];
-            i++;
-        }
 
-        for (i = 0; i < s2l; i++) {
-            if(s2.charAt(i) == s3.charAt(i)){
-                dp[0][i+1] = dp[0][i]+1;
-            }else{
-               break;
-            }
-        }
-        while(i < s2l){
-            dp[0][i+1] = dp[0][i];
-            i++;
-        }
-
-
-
-        for (i = 1; i < s1l+1 ; i++) {
-            for (int j = 1; j < s2l+1 ; j++) {
-                dp[i][j] = Math.max(s3.charAt(dp[i-1][j])==s1.charAt(i-1)?dp[i-1][j]+1:dp[i-1][j],
-                        s3.charAt(dp[i][j-1])==s2.charAt(j-1)?dp[i][j-1]+1:dp[i][j-1]);
-            }
-        }
-        return s3.length() == dp[dp.length-1][dp[0].length-1];
-
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        String s1 = "aabcc";
-        String s2 = "dbbca";
-        String s3 = "aadbbcbcac";
-        System.out.println(solution.isInterleave(s1,s2,s3));
+        return dp[s1.length()][s2.length()];
     }
 }
