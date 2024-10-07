7f2b18702f04bf73bcd09100147b6747b7763e54
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-10-06 23:30:58 -0400

    Time: 0 ms (100%), Space: 42.8 MB (96.75%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        for (int i : nums) {
+            if (i == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
