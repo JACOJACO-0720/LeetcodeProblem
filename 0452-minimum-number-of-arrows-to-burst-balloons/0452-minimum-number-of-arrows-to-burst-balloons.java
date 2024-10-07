998bb0a451072a6fbfbfff67376a313e0b57c504
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-10-07 13:57:40 -0400

    Time: 53 ms (65.86%), Space: 68.8 MB (67.96%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,27 +1,16 @@
 class Solution {
-    public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
-        int result = 1;
-        int length = points.length;
-        int i = 0;
-        int start;
-        int end = points[0][1];
-        while (i < length-1) {
-            i++;
-            start = points[i][0];
-            end = Math.min(points[i][1], end);
-            if (start > end) {
-                result++;
-                end = points[i][1];
+    public int findMinArrowShots(int[][] arr) {
+        int n = arr.length;
+        Arrays.sort(arr,(a,b)->(Integer.compare(a[1],b[1])));
+        int prev[] = arr[0];
+        int ans = 1;
+        for(int i=1; i<n; i++){
+            if(prev[1]>=arr[i][0]) continue;
+            else{
+                ans++;
+                prev = arr[i];
             }
         }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        //int[][] points = new int[][]{{10,16},{2,8},{1,6},{7,12}};
-        int[][] points = new int[][]{{1, 2}, {3, 4}, {5, 6}, {7, 8}};
-        Solution solution = new Solution();
-        System.out.println(solution.findMinArrowShots(points));
+        return ans;
     }
 }
