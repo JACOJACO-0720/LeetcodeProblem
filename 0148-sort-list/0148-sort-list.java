5f50be52677b8f2bc8a09fff746f63d76b0031aa
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-10-06 20:45:48 -0400

    Time: 9 ms (94.56%), Space: 57 MB (36.9%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,78 +10,56 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head==null ||head.next ==null){
+        // 处理空链表或只有一个节点的链表
+        if (head == null || head.next == null) {
             return head;
         }
-        ListNode slow  = head;
+
+        // 使用快慢指针找到链表的中间节点
+        ListNode slow = head;
         ListNode fast = head.next;
-        while(fast!=null&&fast.next!=null){
+        while (fast != null && fast.next != null) {
             slow = slow.next;
-            fast = fast.next;
-            fast= fast.next;
+            fast = fast.next.next;
         }
-        ListNode secondhead = slow.next;
+
+        // 分割链表
+        ListNode secondHead = slow.next;
         slow.next = null;
-        
-        return merge(sortList(head),sortList(secondhead)) ;
-    }
-    
-    ListNode merge(ListNode n1, ListNode n2){
-        ListNode head =null;
-        ListNode current = null;
-        while(n1!=null&&n2!=null){
-            if(head==null){
 
-                if(n1.val<n2.val){
-                    head = n1;
-                    n1=n1.next;
-                    head.next=null;
-                    current = head;
-                    
-                }else{
-                    head = n2;
-                    n2=n2.next;
-                    head.next=null;
-                    current = head;
-                }
+        // 递归排序两部分
+        ListNode left = sortList(head);
+        ListNode right = sortList(secondHead);
 
-            }else{
-                 if(n1.val<n2.val){
-                    current.next = n1;
-                    n1 = n1.next;
-                    current=current.next;
-                    current.next =null;
-                }else{
-                    current.next = n2;
-                    n2 = n2.next;
-                    current=current.next;
-                    current.next =null;
-                }
-            }
-        }
-        if(n1==null&&n2==null){
-            return head;
-        }else if(n2==null){
-            while(n1!=null){
-                    current.next = n1;
-                    n1 = n1.next;
-                    current=current.next;
-                    
-                    current.next =null;
-            }
-
-        }else{
-                while(n2!=null){
-                    current.next = n2;
-                    n2 = n2.next;
-                    current=current.next;
-                    current.next =null;
-                   
-            }
-        }
-        return head;
+        // 合并排序后的两部分
+        return merge(left, right);
     }
 
+    private ListNode merge(ListNode l1, ListNode l2) {
+        // 哨兵节点（dummy node），简化链表的操作
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
 
+        // 合并两个链表
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
 
+        // 处理剩下的节点
+        if (l1 != null) {
+            current.next = l1;
+        } else {
+            current.next = l2;
+        }
+
+        // 返回合并后的链表
+        return dummy.next;
+    }
 }
