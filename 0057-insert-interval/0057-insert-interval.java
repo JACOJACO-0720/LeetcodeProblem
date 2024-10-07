8bac9364103d6614ece35ecde31bf9dae0adf2a9
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-10-07 13:59:34 -0400

    Time: 2 ms (39.55%), Space: 45 MB (29.47%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,50 +1,30 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        if(intervals.length==0){
-            int[][] temp =new int[1][2];
-            temp[0]=newInterval;
-            return temp;
-        }
-        List<int[]> res = new ArrayList<>();
-        Arrays.sort(intervals,(n1,n2)->(Integer.compare(n1[0], n2[0])));
-        int cur = 0;
-        while(cur<intervals.length && newInterval[0]>intervals[cur][1]){
-            res.add(intervals[cur]);
-            cur++;
-        }
-        if(cur>=intervals.length){
-            res.add(newInterval);
-             return res.toArray(new int[res.size()][]);
-        }
-        boolean haschange = false;
-        if(cur<intervals.length && newInterval[1]>=intervals[cur][0]){
-            haschange=true;
-            int[] newarray = new int[2];
-            newarray[0] = Math.min(newInterval[0],intervals[cur][0]);
-            newarray[1] = Math.max(newInterval[1],intervals[cur][1]);
-            cur++;
-            if(cur>=intervals.length){
-                 res.add(newarray);
-                  return res.toArray(new int[res.size()][]);
-            }
-            while(cur<intervals.length&&intervals[cur][0]<=newarray[1]){
-                newarray[0] = Math.min(newarray[0],intervals[cur][0]);
-                newarray[1] = Math.max(newarray[1],intervals[cur][1]);
-                cur++;
-            }
-            res.add(newarray);
-        }
-        if(res.isEmpty()||!haschange){
-            res.add(newInterval);
-        }
-        while(cur<intervals.length && cur<intervals.length){
-            res.add(intervals[cur]);
-            cur++;
-        }
-        return res.toArray(new int[res.size()][]);
+public int[][] insert(int[][] intervals, int[] newInterval) {
+		List<int[]> result = new LinkedList<>();
+	    int i = 0;
+	    // add all the intervals ending before newInterval starts
+	    while (i < intervals.length && intervals[i][1] < newInterval[0]){
+	        result.add(intervals[i]);
+	        i++;
+	    }
+	    
+	    // merge all overlapping intervals to one considering newInterval
+	    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+	    	// we could mutate newInterval here also
+	        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+	        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+	        i++;
+	    }
+	    
+	    // add the union of intervals we got
+	    result.add(newInterval); 
+	    
+	    // add all the rest
+	    while (i < intervals.length){
+	    	result.add(intervals[i]); 
+	    	i++;
+	    }
+	    
+	    return result.toArray(new int[result.size()][]);
     }
 }
