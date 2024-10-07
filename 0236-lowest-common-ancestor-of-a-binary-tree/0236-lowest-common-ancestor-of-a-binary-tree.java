bfcdd05d332137603cc72f8fb422795bb5a97949
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-10-06 21:06:00 -0400

    Time: 7 ms (63.39%), Space: 44.3 MB (96.46%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,24 +9,23 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // Base case
-        if (root == null || root == p || root == q) {
+        if (root == null){
+            return null;
+        } 
+        if(root == p || root == q) {
             return root;
         }
-
-        // Recur for left and right subtrees
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-        // If both left and right are not null, then one key
-        // is present in one subtree and the other is present
-        // in the other subtree. So this node is the LCA.
         if (left != null && right != null) {
             return root;
         }
-
-        // Otherwise check if left subtree or right subtree
-        // is LCA
-        return (left != null) ? left : right;
+        if(left!=null){
+            return left;
+        }
+        if(right!=null){
+            return right;
+        }
+        return null;
     }
 }
