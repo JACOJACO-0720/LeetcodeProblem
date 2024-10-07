459a576200e1930a5388c7d6ad6738c2b999eb99
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-10-07 13:09:02 -0400

    Time: 154 ms (42.64%), Space: 41.5 MB (65.25%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,46 +1,36 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int rows = board.length;
-        int cols = board[0].length;
-        // 特殊情况处理
-        if (word.length() == 0) return true;
-        if (rows * cols < word.length()) return false;
 
-        boolean[][] visited = new boolean[rows][cols];
-        // 遍历每个起始点
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                // 如果找到匹配的首字母，开始DFS
-                if (dfs(board, word, i, j, 0, visited)) {
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                boolean[][] visited = new boolean[board.length][board[0].length];
+                boolean res = dfs(visited, i, j, board,word,0);
+                if(res){
                     return true;
                 }
             }
         }
         return false;
     }
-
-    boolean dfs(char[][] board, String word, int x, int y, int index, boolean[][] visited) {
-        // 超出边界或已访问或字符不匹配，返回false
-        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length ||
-            visited[x][y] || board[x][y] != word.charAt(index)) {
+    
+    boolean dfs(boolean[][] visited, int i ,int j, char[][] board,String word, int cur){
+        if(outofbound(i,j, board) ||visited[i][j] ||  word.charAt(cur)!=board[i][j]){
             return false;
         }
-
-        // 全部字符匹配，返回true
-        if (index == word.length() - 1) {
+        if(cur==word.length()-1){
             return true;
+        }else{
+            visited[i][j] = true;
+            boolean res =  dfs(visited, i+1, j, board,word,cur+1)
+                    ||dfs(visited, i-1, j, board,word,cur+1)
+                    ||dfs(visited, i, j+1, board,word,cur+1)
+                    ||dfs(visited, i, j-1, board,word,cur+1);
+            visited[i][j] = false;
+            return res;
         }
-
-        visited[x][y] = true; // 标记为已访问
-
-        // 递归搜索上下左右四个方向
-        boolean res = dfs(board, word, x + 1, y, index + 1, visited) ||
-                      dfs(board, word, x - 1, y, index + 1, visited) ||
-                      dfs(board, word, x, y + 1, index + 1, visited) ||
-                      dfs(board, word, x, y - 1, index + 1, visited);
-
-        visited[x][y] = false; // 回溯，取消标记
-
-        return res;
+    }
+    boolean outofbound(int i ,int j, char[][] board){
+        return i<0||j<0||i>=board.length||j>=board[0].length;
     }
 }
