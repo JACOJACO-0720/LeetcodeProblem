767ab25ee60d831f893437260a39bbc1c76cb198
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-10-07 12:42:53 -0400

    Time: 0 ms (100%), Space: 42 MB (67.7%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,32 +2,29 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int left = 0; int right = matrix.length-1;
         while(left<=right){
-            int mid = left+(right-left)/2;
-            if(matrix[mid][0]<=target && matrix[mid][matrix[0].length-1]>=target){
-                return subcheck(mid, matrix, target);
+            int mid = left +(right-left)/2;
+            if(target>=matrix[mid][0] && target<=matrix[mid][matrix[0].length-1]){
+                return searchInSingleRow(matrix[mid], target);
+            }else if(target < matrix[mid][0]){
+                right = mid-1;
+
             }else{
-                if(matrix[mid][matrix[0].length-1]<target){
-                    left = mid+1;
-                }else{
-                    right= mid-1;
-                }
+                left = mid+1;
             }
         }
         return false;
+
     }
-    boolean subcheck(int line, int[][] matrix, int target){
-        int left = 0; int right = matrix[0].length-1;
-        
+    boolean searchInSingleRow(int[] matrix, int target){
+        int left = 0; int right = matrix.length-1;
         while(left<=right){
-            int mid = left+(right-left)/2;
-            if(matrix[line][mid]==target){
+            int mid = left +(right-left)/2;
+            if(target==matrix[mid]){
                 return true;
+            }else if(target<=matrix[mid]){
+                right=mid-1;
             }else{
-                if(matrix[line][mid]>target){
-                    right = mid-1;
-                }else{
-                    left = mid+1;
-                }
+                left = mid+1;
             }
         }
         return false;
