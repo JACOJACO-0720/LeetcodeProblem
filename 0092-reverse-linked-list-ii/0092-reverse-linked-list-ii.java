1ae199fdf306791407d313e5921c19f143ed1a04
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-12-22 22:51:38 +0800

    Time: 0 ms (100%), Space: 41.7 MB (12.62%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -1,32 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         ListNode dummyhead = new ListNode(-501);
         dummyhead.next = head;
         ListNode beforeleft = dummyhead;
-
-        // Step 1: Find the node just before the "left" position
-        for (int i = 0; i < left - 1; i++) {
+        for(int i =0;i<left-1;i++){
             beforeleft = beforeleft.next;
         }
-
-        // Step 2: Identify the node at the "left" position
         ListNode leftNode = beforeleft.next;
+        beforeleft.next =null;
+
         ListNode one = null;
         ListNode two = leftNode;
         ListNode temp = null;
-
-        // Step 3: Reverse the sublist from "left" to "right"
-        for (int i = 0; i < right - left + 1; i++) {
-            temp = two.next;  // Store next node
-            two.next = one;   // Reverse the link
-            one = two;        // Move one step forward in the reversed list
-            two = temp;       // Move to the next node in the original list
+        for(int i =0;i<right-left+1;i++){
+            if(one==null){
+                one = two;
+                two = two.next;
+                one.next = null;
+            }else{
+                temp = two.next;
+                two.next = one;
+                one = two;
+                two = temp;
+            }
         }
-
-        // Step 4: Connect the reversed sublist back to the original list
-        beforeleft.next = one;  // Connect the node before "left" to the new head
-        leftNode.next = two;    // Connect the original "left" node to the node after "right"
-
-        return dummyhead.next;  // Return the head of the modified list
+        beforeleft.next = one;
+        leftNode.next = two;
+        return dummyhead.next;
     }
 }
