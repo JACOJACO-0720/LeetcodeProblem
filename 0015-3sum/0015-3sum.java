05e2a24b1c3dc3733e23cf46e4dee53c5bd5a96c
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-12-22 09:52:09 +0800

    Time: 25 ms (95.84%), Space: 52.8 MB (14.32%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,47 +1,40 @@
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer> > res = new ArrayList<>();
+        List<List<Integer>> resLst = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length-2; i++) {
-            if(i!=0 && nums[i]==nums[i-1]){
+            if(i!=0 && nums[i] == nums[i-1]){
                 continue;
             }
-            int first = nums[i];
-            
-            int left = i+1;
-            int right = nums.length-1;
-            int target = -1*first;
-            while(left<right){
-                int tempsum = nums[left] + nums[right];
-                if(tempsum==target){
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(first);temp.add( nums[left]);temp.add(nums[right]);
-                    res.add(temp);
-
-                    left++;
-                    while(left<nums.length && nums[left]==nums[left-1]){
-                        left++;
-                    }
-
-                }else if(tempsum<target){
-                    left++;
-                    while(left<nums.length && nums[left]==nums[left-1]){
-                        left++;
-                    }
-                }else{
-                    right--;
-                    while(right>=0 && nums[right]==nums[right+1]){
-                        right--;
-                    }
-                }
-
+            int one = nums[i];
+            if(one>0){
+                break;
             }
+            int two = i+1;
+            int three = nums.length-1; 
+            while(two<three){
+                
+                if(one + nums[two]+ nums[three] ==0){
+                    ArrayList<Integer> al = new ArrayList<>();
+                    al.add(one);al.add(nums[two]);al.add(nums[three]);
+                    resLst.add(al);
+                    two++;
+                    three--;
+                    while(two<three && nums[two]==nums[two-1]){
+                        two++;
+                    }
+                    while(two<three && nums[three]== nums[three+1]){
+                        three--;
+                    }
+                }else if(one + nums[two]+ nums[three] >0){
+                    three--;
+                }else{
+                    two++;
+                }
+            }
+
         }
-        return res;
+
+        return resLst;
     }
 }
