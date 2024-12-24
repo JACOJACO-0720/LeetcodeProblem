83ffd5367c021147872dc38ea5cfd95fa7194151
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2024-12-24 12:20:08 +0800

    Time: 6 ms (74.68%), Space: 45.5 MB (49.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,48 +1,48 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if(prerequisites.length==1){
-            return true;
-        }
-        int remain = numCourses;
-        List<List<Integer>> adjList = new ArrayList<>();
-        List<List<Integer>> revList = new ArrayList<>();
+        // 正向图和入度数组
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] inDegree = new int[numCourses];
+
+        // 初始化邻接表
         for (int i = 0; i < numCourses; i++) {
-            List<Integer> temp1 = new ArrayList<>();
-            List<Integer> temp2 = new ArrayList<>();
-            adjList.add(temp1);
-            revList.add(temp2);
+            graph.add(new ArrayList<>());
         }
 
-
-        for (int[] prerequisite: prerequisites) {
-            adjList.get(prerequisite[0]).add(prerequisite[1]);
-            revList.get(prerequisite[1]).add(prerequisite[0]);
+        // 构建图和入度数组
+        for (int[] prereq : prerequisites) {
+            int course = prereq[0];
+            int prerequisite = prereq[1];
+            graph.get(prerequisite).add(course); // prerequisite -> course
+            inDegree[course]++; // 增加入度
         }
 
-        ArrayDeque<Integer> ad = new ArrayDeque<>();
-        for(int i =0;i< adjList.size();i++){
-            if(adjList.get(i).isEmpty()){
-                ad.add(i);
+        // 使用队列存储入度为0的节点
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i); // 将所有入度为0的课程放入队列
             }
         }
-        while(!ad.isEmpty()){
-            int temp = ad.poll();
-            remain--;
-            List<Integer> rev = revList.get(temp);
 
-            for (int i = 0; i < rev.size(); i++) {
-                adjList.get(rev.get(i)).remove(Integer.valueOf(temp));
-                if(adjList.get(rev.get(i)).isEmpty()){
-                    ad.add(rev.get(i));
+        // 拓扑排序
+        int completedCourses = 0; // 已完成的课程数
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            completedCourses++;
+
+            // 遍历邻接课程，减少入度
+            for (int nextCourse : graph.get(course)) {
+                inDegree[nextCourse]--;
+                if (inDegree[nextCourse] == 0) {
+                    queue.offer(nextCourse); // 如果某课程的入度为0，加入队列
                 }
             }
-
         }
-        return remain == 0;
 
+        // 如果完成的课程数等于总课程数，则可以完成所有课程
+        return completedCourses == numCourses;
     }
 }
