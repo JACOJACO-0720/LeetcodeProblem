5f2c3ef46bed8bf1f2005ffbe07ab87efdc049ae
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2025-01-01 16:58:45 +0800

    Time: 97 ms (31.81%), Space: 62.9 MB (51.17%) - LeetHub

---
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            graph.putIfAbsent(manager[i], new ArrayList<>());
+            graph.get(manager[i]).add(i);
+        }
+        return dfs(graph, headID, informTime);
+    }
+
+    int dfs(Map<Integer, List<Integer>> graph, int curHead, int[] informTime) {
+        int curMax = 0;
+        if(!graph.containsKey(curHead)){
+            return curMax;
+        }
+        for(int subordinate : graph.get(curHead)) {
+            curMax = Math.max(curMax, dfs(graph, subordinate, informTime));
+        }
+        return curMax + informTime[curHead];
+    }
+}
