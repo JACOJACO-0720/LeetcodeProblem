d05ece1863179433cc322acae3ad369eac3a8dab
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2025-01-09 16:38:52 -0500

    Time: 12 ms (13.97%), Space: 64.8 MB (5.71%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<String>> partition(String s) {
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j <=i; j++) {
+                if(i-j>=0 && s.charAt(i)==s.charAt(i-j) && ((j-2>=0 && dp[i-1][j-2])||j==1)){
+                    dp[i][j] = true;
+                }
+            }
+        }
+        
+        List<List<String>> res = new ArrayList<>();
+        dfs(res, s, 0, new ArrayList<>(), dp);
+        return res;
+
+    }
+    void dfs( List<List<String>> res, String s, int start ,List<String> temp, boolean[][] dp){
+        if(start==s.length()){
+            List<String> t = new ArrayList<>();
+            for (int i = 0; i < temp.size(); i++) {
+                String tmps = new String(temp.get(i));
+                t.add(tmps);
+            }
+            res.add(t);
+            return;
+        }
+        for (int i = start; i < s.length(); i++) {
+            if(dp[i][i-start]){
+                temp.add(s.substring(start,i+1));
+                dfs(res, s, i+1, temp, dp);
+                temp.remove(temp.size()-1);
+            }
+        }
+        
+    }
+}
