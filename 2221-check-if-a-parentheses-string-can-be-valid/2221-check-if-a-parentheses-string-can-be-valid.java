456390f469f306882a7f7e6b7846bf634ce3b6c0
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2025-01-13 14:07:03 -0500

    Time: 65 ms (9.68%), Space: 55.5 MB (5.65%) - LeetHub

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean canBeValid(String s, String locked) {
+        int[][] dp = new int[s.length()+1][2]; //min max
+        dp[0][0] = 0;
+        dp[0][1] = 0;
+        
+        for(int i =0;i< s.length();i++){
+            char islocked = locked.charAt(i);
+            char par = s.charAt(i);
+            if(islocked == '1'){
+                if(par == '('){
+                    dp[i+1][1] = dp[i][1]+1;
+                    dp[i+1][0] = dp[i][0]+1;
+                }else{
+                    dp[i+1][1] = dp[i][1]-1;
+                    dp[i+1][0] = dp[i][0]-1;
+                    if(dp[i+1][0]==-1){
+                        dp[i+1][0] = 1;
+                    }
+                    if(dp[i+1][1] <0 || dp[i+1][0]>dp[i+1][1]){
+                        return false;
+                    }
+                }
+            }else{
+                    dp[i+1][1] = dp[i][1]+1;
+                    dp[i+1][0] = dp[i][0]-1;
+                    if(dp[i+1][0]==-1){
+                        dp[i+1][0] = 1;
+                    }
+                    if(dp[i+1][1] <0 || dp[i+1][0]>dp[i+1][1]){
+                        return false;
+                    }
+
+            }
+        }
+        if(dp[dp.length-1][1]>=0 && dp[dp.length-1][0]<=0){
+            return true;
+        }
+        return false;
+
+
+        
+    }
+}
