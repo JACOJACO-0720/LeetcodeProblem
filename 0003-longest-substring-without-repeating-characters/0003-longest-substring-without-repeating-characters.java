91fcff31f1830e5b3c429c51bc8a32e69893685e
JACO-0720 <147264316+JACOJACO-0720@users.noreply.github.com>
2025-01-21 21:34:24 -0500

    Time: 6 ms (70%), Space: 45 MB (31.14%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,45 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.isEmpty()){
+        if(s==null || s.isEmpty()){
             return 0;
         }
-        if(s.length()==1){
-            return 1;
+        int left = 0; int right = 0;
+        int max = 1;
+        HashSet<Character> hs  = new HashSet<>();
+        while(left<s.length()&&right<s.length()){
+            char tmpchar = s.charAt(right);
+            while(left<right && hs.contains(tmpchar)){
+                hs.remove(s.charAt(left));
+                left++;
+            }
+            hs.add(tmpchar);
+            max = Math.max(right-left+1,max ); 
+            right++;
         }
-
-        int res = 1;
-        int left = 0;
-        int right = 0;
-        int current = 1;
-        int[] table = new int[200];
-        table[s.charAt(0)] = 1;
-        while(right<s.length()){
-                if(right+1 < s.length() && table[s.charAt(right+1)]!=0){
-                    if(left==right){
-                        table[s.charAt(right)] = 0;
-                        table[s.charAt(right+1)] = 1;
-                        left++;
-                        right++;
-                        current = 1;
-                    }else{
-                        table[s.charAt(left)]= 0;
-                        left++;
-                        current--;
-                    }
-                }else{
-                    if(right+1==s.length()){
-                        return res;
-                    }else{
-                        table[s.charAt(right+1)]=1;
-                        current++;
-                        right++;
-                        res = Math.max(res,current);
-                    }
-
-                }
-            
-
-        }
-        return res;
+        return max;
     }
 }
